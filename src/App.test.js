c53f95ad1aad9d88d9c17b7f076e71aeacc30aa4
inmax <inmaculadaminarroruiz@gmail.com>
2020-05-24 13:52:58 +0200

    test completo de app

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import App, { Todo, TodoForm, useTodos } from "./App";
-import { shallow, configure } from "enzyme";
+import { shallow, configure, mount } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
 configure({ adapter: new Adapter() });
@@ -135,8 +135,8 @@ describe("App", () => {
       props.removeTodo(0);
       //Ahora el valor inicial que tenía props ha cambiado, por eso necesito volver a llamarlo
       props = wrapper.find("div").props();
-      console.log(props.todos[0])
-      expect(props.todos[0]).toEqual({ text: "Todo 2",isCompleted: false });
+      console.log(props.todos[0]);
+      expect(props.todos[0]).toEqual({ text: "Todo 2", isCompleted: false });
     });
 
     describe("completeTodo", () => {
@@ -155,7 +155,24 @@ describe("App", () => {
       props.completeTodo(0);
       //Ahora el valor inicial que tenía props ha cambiado, por eso necesito volver a llamarlo
       props = wrapper.find("div").props();
-      expect(props.todos[0]).toEqual({ text:"Todo 1",isCompleted: true });
+      expect(props.todos[0]).toEqual({ text: "Todo 1", isCompleted: true });
     });
+  });
+  // Test a la madre del cordero
+  describe("App", () => {
+    //1-Montado del componente
+    const wrapper = mount(<App />);
+    //2- Crear un espía
+    const prevent = jest.fn();
+
+    wrapper.find("input").simulate("change", { target: { value: "mi todo!" } });
+
+    wrapper.find("form").simulate("submit", { preventDefault: prevent });
+
+    const res= wrapper.find('.todo').at(0).text().includes('mi todo!')
+
+    expect(res).toEqual(true);
+    expect(prevent.mock.calls).toEqual([[]]);
+    
   });
 });
